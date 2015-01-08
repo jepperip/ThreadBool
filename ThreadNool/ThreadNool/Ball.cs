c48c20d73bab7b877986e80ec35c794ff3f0364d
MrCuddle <simondahlberg89@gmail.com>
2015-01-08 10:42:20 +0100

    BALLS BALLS BALLS BALLS

--- ThreadNool/ThreadNool/Ball.cs
+++ ThreadNool/ThreadNool/Ball.cs
@@ -2,10 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ThreadNool
 {
     class Ball
     {
+      int radius;
+      Vector2 position;
+      Texture2D texture;
+      Color color;
+
+      public Ball(Vector2 initPos, Color color)
+      {
+        radius = 16;
+        position = initPos;
+        this.color = color;
+      }
+
+      public Vector2 GetCenter()
+      {
+        return new Vector2(position.X + radius, position.Y + radius);
+      }
+
+      public void Draw(SpriteBatch sb)
+      {
+        sb.Draw(texture, position, color);
+      }
     }
 }
