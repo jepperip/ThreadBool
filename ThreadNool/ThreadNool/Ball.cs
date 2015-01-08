ab4a25d9a84b2d219bcf106a9d0374eb3c66e563
MrCuddle <simondahlberg89@gmail.com>
2015-01-08 12:34:25 +0100

    sdsda
    asdsda

--- ThreadNool/ThreadNool/Ball.cs
+++ ThreadNool/ThreadNool/Ball.cs
@@ -30,7 +30,11 @@ namespace ThreadNool
         {
             return new Vector2(position.X + radius, position.Y + radius);
         }
-
+        /// <summary>
+        /// Updates the ball. If the ball is not already selected it checks if the mouse was clicked inside of the ball, if so the state of the ball and the colour is changed to indicate selection
+        /// Else the ball was previously selected and the direction where the ball is supposed to go is calculated. Lastly the direction is added on to the balls position.
+        /// </summary>
+        /// <param name="clickPos">The point where the mouse was clicked</param>
         public void Update(Point clickPos)
         {
             if (clickPos.X != -1 && clickPos.Y != -1)
